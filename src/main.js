0f1ce97db1d7b83ec6f41bf707d976cfc235eee6
KsandrKam <alexandr.kamkov2012@gmail.com>
2022-12-15 13:26:32 +0300

    1.11

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 import FilterView from './view/filters-view.js';
 import SortView from './view/sort-view.js';
 import BoardView from './view/board-view.js';
-import TaskView from './view/point-view.js';
+//import TaskView from './view/point-view.js';
 import TaskEditView from './view/task-edit-view.js';
 import {render} from './render.js';
 import BoardPresenter from './presenter/presenter.js';
 
-const siteMainElement = document.querySelector('.page-body__page-main');
-const siteHeaderElement = document.querySelector('.page-header');
+const siteMainElement = document.querySelector('.trip-events');
+const siteHeaderElement = document.querySelector('.trip-controls__filters');
 const boardPresenter = new BoardPresenter({boardContainer: siteMainElement});
 
 //render(new FormEditView(), siteHeaderElement);
@@ -16,7 +16,7 @@ render(new FilterView(), siteHeaderElement);
 render(new SortView(), siteMainElement);
 render(new TaskEditView(), siteMainElement);
 render(new BoardView(), siteMainElement);
-render(new TaskView(), siteMainElement);
+//render(new TaskView(), siteMainElement);
 
 
 boardPresenter.init();
