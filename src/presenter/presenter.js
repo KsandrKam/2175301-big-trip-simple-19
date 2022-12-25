d39c881ca293b3b52ba77dec723cf8136e12bc1f
KsandrKam <alexandr.kamkov2012@gmail.com>
2022-12-25 15:24:45 +0300

    Революция или эволюция? (часть 1)

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,31 +1,71 @@
 import SortView from '../view/sort-view.js';
 import TripEditView from '../view/trip-edit-view.js';
 import NewPointView from '../view/create-new-point-view.js';
-import TripListView from '../view/trip-list-view.js';
 import PointView from '../view/point-view.js';
 import {render, RenderPosition} from '../render.js';
-
-
-const siteMainElement = document.querySelector('.trip-events');
+import PointListView from '../view/point-list-view.js';
+import { isEscapeKey } from '../utils';
 export default class TripPresenter {
-  tripComponent = new TripListView();
-
-  constructor({tripContainer, pointsModel}) {
-    this.tripContainer = tripContainer;
-    this.pointsModel = pointsModel;
+  #pointListComponent = new PointListView();
+  #pointContainer = null;
+  #pointsModel = null;
+  #listPoints = [];
+  constructor({pointContainer, pointsModel}) {
+    this.#pointContainer = pointContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.listPoints = [...this.pointsModel.getPoints()];
-    render(new SortView(), siteMainElement);
-    render(this.tripComponent, siteMainElement);
+    this.#listPoints = [...this.#pointsModel.points];
 
-    render(new NewPointView(), this.tripComponent.getElement(), RenderPosition.AFTERBEGIN);
-    render(new TripEditView(), this.tripComponent.getElement(), RenderPosition.AFTERBEGIN);
+    render(new SortView(), this.#pointContainer);
+    render(this.#pointListComponent, this.#pointContainer);
+    render(new NewPointView(), this.#pointListComponent.element, RenderPosition.AFTERBEGIN);
 
-    for (let i = 0; i < this.listPoints.length; i++) {
-      render(new PointView({point: this.listPoints[i]}), this.tripComponent.getElement());
-    }
-
+    this.#listPoints.forEach((point) => this.#renderPoint(point));
   }
+
+  #renderPoint(point) {
+    const pointComponent = new PointView({point});
+    const pointEditComponent = new TripEditView({point});
+
+    const pointRollupBtn = pointComponent.element.querySelector('.event__rollup-btn');
+    const editPointForm = pointEditComponent.element.querySelector('form');
+    const editRollupBtn = editPointForm.querySelector('.event__rollup-btn');
+
+    const replacePointToEditForm = () => {
+      this.#pointListComponent.element.replaceChild(pointEditComponent.element, pointComponent.element);
+    };
+    const replaceEditFormToPoint = () => {
+      this.#pointListComponent.element.replaceChild(pointComponent.element, pointEditComponent.element);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (isEscapeKey) {
+        evt.preventDefault();
+        replaceEditFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    editRollupBtn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      replaceEditFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    pointRollupBtn.addEventListener('click', () => {
+      replacePointToEditForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    editPointForm.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceEditFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+    render(pointComponent, this.#pointListComponent.element);
+  }
+
 }
+
