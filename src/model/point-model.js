1ca804fb0fb8490c6dc842ac91cc792e330b107d
KsandrKam <alexandr.kamkov2012@gmail.com>
2023-01-16 22:02:06 +0300

    3.2

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,6 +1,6 @@
 import { getRandomPoint } from '../constants/point.js';
 
-const POINTS = 4;
+const POINTS = 3;
 
 export default class PointModel {
   #point = Array.from({length: POINTS}, getRandomPoint);
